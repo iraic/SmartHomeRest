3038c32c4773713223800fe67a6c2954598946ff
Iraic <jiraic@cdhidalgo.tecnm.mx>
2023-02-17 10:30:40 -0600

    login y jwt

--- Program.cs
+++ Program.cs
@@ -1,29 +1,107 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
+/******************* Builder *************************************************************/
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
 //builder.Services.AddDbContext<SmarthomeContext>(options => 
 //    options.UseInMemoryDatabase("SensorsList"));
 builder.Services.AddDbContext<SmarthomeContext>(options =>
-    options.UseMySQL("Server=localhost;Database=smarthome;Uid=root;Password="));
+    options.UseMySQL(builder.Configuration["ConnectionStrings:MySql"]));
 
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JSON Web Token based security",
+};
+
+var securityReq = new OpenApiSecurityRequirement()
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] {}
+    }
+};
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "Mi SamartHome API", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "Mi SmartHome API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", securityScheme);
+    c.AddSecurityRequirement(securityReq);
 });
 
+/**********************  APP *****************************************************/
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", () => "Mi SamartHome API");
-app.MapGet("/sensores", async (SmarthomeContext db) =>
+/***************************************************************************************/
+app.MapGet("/", [AllowAnonymous] () => "Mi SmartHome API");
+
+app.MapPost("/login", [AllowAnonymous] async (User user, SmarthomeContext db) =>
+{
+    var userdb = await db.Users.FindAsync(user.Username);
+    if(userdb is null) return Results.NotFound(user.Username);
+    if (userdb.Password != user.Password) return Results.Unauthorized();
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+    var jwtTokenHandler = new JwtSecurityTokenHandler();
+    var descriptor = new SecurityTokenDescriptor()
+    {
+        SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256),
+        Expires = DateTime.UtcNow.AddHours(1)
+    };
+    var token = jwtTokenHandler.CreateToken(descriptor);
+    var jwtToken = jwtTokenHandler.WriteToken(token);
+    return Results.Ok(jwtToken);
+});
+
+/************************* CRUD *****************************************************/
+app.MapGet("/sensores", [Authorize] async (SmarthomeContext db) =>
 {
     return await db.Sensors.ToListAsync();
 });
 
-app.MapGet("/sensores/{id}", async (int id, SmarthomeContext db) =>
+app.MapGet("/sensores/{id}", [Authorize] async (int id, SmarthomeContext db) =>
 {
     var sensor = await db.Sensors.FindAsync(id);
     if (sensor is null)
@@ -33,7 +111,7 @@ app.MapGet("/sensores/{id}", async (int id, SmarthomeContext db) =>
     return Results.Ok(sensor);
 });
 
-app.MapPost("/sensores", async (Sensor s, SmarthomeContext db) =>
+app.MapPost("/sensores", [Authorize] async (Sensor s, SmarthomeContext db) =>
 {
     s.Date = DateTime.Now;
     db.Sensors.Add(s);
@@ -41,7 +119,7 @@ app.MapPost("/sensores", async (Sensor s, SmarthomeContext db) =>
     return Results.Created($"/sensores/{s.Id}", s);
 });
 
-app.MapPut("/sensores/{id}", async (int id, Sensor s, SmarthomeContext db) =>
+app.MapPut("/sensores/{id}", [Authorize] async (int id, Sensor s, SmarthomeContext db) =>
 {
     var sensor = await db.Sensors.FindAsync(id);
     if (sensor is null)
@@ -54,7 +132,7 @@ app.MapPut("/sensores/{id}", async (int id, Sensor s, SmarthomeContext db) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/sensores/{id}", async (int id, SmarthomeContext db) =>
+app.MapDelete("/sensores/{id}", [Authorize] async (int id, SmarthomeContext db) =>
 {
     var sensor = await db.Sensors.FindAsync(id);
     if (sensor is null)
@@ -65,13 +143,21 @@ app.MapDelete("/sensores/{id}", async (int id, SmarthomeContext db) =>
     await db.SaveChangesAsync();
     return Results.NoContent();
 });
+/*****************************************************************************************/
 
 app.Run();
 
+class User
+{
+    [Key]
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
 class Sensor
 {
+    [Key]
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public double Value { get; set; }
     public DateTime Date { get; set; }
 }
@@ -79,6 +165,7 @@ class Sensor
 class SmarthomeContext : DbContext
 {
     public DbSet<Sensor> Sensors => Set<Sensor>();
+    public DbSet<User> Users => Set<User>();
     public SmarthomeContext(DbContextOptions<SmarthomeContext> options) : base(options)
     {
     }
